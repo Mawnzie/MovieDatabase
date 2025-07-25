32b82e46b3b0eb40001e5c7676826af82b7bcd2f
majaw <majaw@Mac-2.lan>
2025-07-25 12:28:08 -0700

    Fixed it so that an image is displayed instead of a byte array

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efbceaec16693291fb6504a8039e584ed293bfaf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6048a16231a705b39678a888b39f49537aad27d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
