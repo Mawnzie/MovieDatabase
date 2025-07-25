732810dfe8d8e3912d1d2680464f967b4a092f67
majaw <majaw@Mac-2.lan>
2025-07-25 14:44:21 -0700

    Fixed cards

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcc6dffa8534b1fb1f8f639eefd412c0c23e6ccd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87c658ce399d25645f6f3a60c877c75c4dfb2e75")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
